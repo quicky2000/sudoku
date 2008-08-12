6af1e707d338826a99ca2ba7877e9039306a9e2b
Julien Thevenon <julien_thevenon@yahoo.fr>
2008-08-12 20:04:36 +0200

    Update to improve situation display

--- src/sudoku_situation.cpp
+++ src/sudoku_situation.cpp
@@ -10,12 +10,28 @@ string sudoku_situation::toString(void)const
 {
   stringstream l_result;
   const string l_horizontal_separator = string(m_big_side_size*4+1,'-');
+  const string l_horizontal_separator_2 = string(m_big_side_size*4+1,'+');
   for(unsigned int l_y = 0; l_y < m_big_side_size ; l_y++)
     {
-      l_result << l_horizontal_separator << endl ;
+      if( l_y % 3)
+	{
+	  l_result << l_horizontal_separator << endl ;
+	}
+      else
+	{
+	  l_result << l_horizontal_separator_2 << endl ;
+	}
+
       for(unsigned int l_x = 0; l_x < m_big_side_size ; l_x++)
 	{
+      if( l_x % 3)
+	{
+	  l_result << "  "  ;
+	}
+      else
+	{
 	  l_result << "| "  ;
+	}
 	  unsigned int l_value = getValue(l_x,l_y);
 	  if(l_value)
 	    {
