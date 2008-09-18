ef0a6a8e7714f5e3e20340f4dbae16dc54e185e2
Julien Thevenon <julien_thevenon@yahoo.fr>
2008-09-19 00:41:13 +0200

    Update to improve transition weight computing

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -99,7 +99,7 @@ sudoku::sudoku(void):FSM<sudoku_situation,sudoku_transition>("sudoku",new sudoku
 
 
 	//Moyen
-	getSituation()->setValue(0,0,3);
+/*	getSituation()->setValue(0,0,3);
 	getSituation()->setValue(4,0,5);
 	getSituation()->setValue(6,0,7);
 
@@ -134,7 +134,44 @@ sudoku::sudoku(void):FSM<sudoku_situation,sudoku_transition>("sudoku",new sudoku
 
 	getSituation()->setValue(2,8,1);
 	getSituation()->setValue(4,8,9);
-	getSituation()->setValue(8,8,7);
+	getSituation()->setValue(8,8,7);*/
+
+	// Diabolique
+	getSituation()->setValue(0,0,3);
+	getSituation()->setValue(5,0,1);
+	getSituation()->setValue(6,0,2);
+
+	getSituation()->setValue(2,1,4);
+	getSituation()->setValue(4,1,8);
+	getSituation()->setValue(6,1,5);
+
+	getSituation()->setValue(0,2,1);
+	getSituation()->setValue(2,2,6);
+	getSituation()->setValue(4,2,7);
+
+	getSituation()->setValue(4,3,3);
+	getSituation()->setValue(5,3,5);
+	getSituation()->setValue(6,3,4);
+	getSituation()->setValue(8,3,6);
+
+	getSituation()->setValue(3,4,9);
+	getSituation()->setValue(6,4,3);
+
+	getSituation()->setValue(5,5,6);
+	getSituation()->setValue(7,5,5);
+
+	getSituation()->setValue(0,6,9);
+	getSituation()->setValue(5,6,8);
+	getSituation()->setValue(8,6,3);
+
+	getSituation()->setValue(0,7,5);
+	getSituation()->setValue(2,7,3);
+	getSituation()->setValue(8,7,4);
+
+	getSituation()->setValue(2,8,1);
+	getSituation()->setValue(5,8,2);
+	getSituation()->setValue(6,8,6);
+
 
 }
 
@@ -197,7 +234,54 @@ void sudoku::computeTransitionWeights(std::vector<FSM_weighted_transition_index_
       pair<unsigned int,unsigned int> l_pair(l_transition->getX(),l_transition->getY());
       map<pair<unsigned int,unsigned int>,unsigned int>::iterator l_iter = l_pair_counter.find(l_pair);
       assert(l_iter != l_pair_counter.end());
-      p_vector.push_back(new FSM_weighted_transition_index(l_index,l_iter->second));
+      unsigned int l_weight = l_iter->second;
+
+      if(l_weight > 1 )
+	{
+	  unsigned char l_count_line = 0;
+	  unsigned char l_count_column = 0;
+	  unsigned char l_count_square = 0;
+	  unsigned int l_square_x = l_transition->getX() / 3;
+	  unsigned int l_square_y = l_transition->getY() / 3;
+	  for(unsigned int l_index2=0;l_index2<9;l_index2++)
+	    {
+	      set<unsigned int> l_result ;
+	      if(!getSituation()->getValue(l_index2,l_transition->getY()))
+		{
+		  l_context->getAuthorizedValues(l_index2,l_transition->getY(),l_result);
+		  if(l_result.count(l_transition->getValue()))
+		    {
+		      l_count_line++;
+		    }
+		  l_result.clear();
+		}
+	      if(!getSituation()->getValue(l_transition->getX(),l_index2))
+		{
+		  l_context->getAuthorizedValues(l_transition->getX(),l_index2,l_result);
+		  if(l_result.count(l_transition->getValue()))
+		    {
+		      l_count_column++;
+		    }
+		  l_result.clear();
+		}
+	      if(!getSituation()->getValue(3*l_square_x+l_index2 /3 ,3*l_square_y + (l_index2 % 3 )))
+		{
+		  l_context->getAuthorizedValues(3*l_square_x+l_index2 /3 ,3*l_square_y + (l_index2 % 3 ),l_result);
+		  if(l_result.count(l_transition->getValue()))
+		    {
+		      l_count_square++;
+		    }
+	      
+		}
+	    }
+	      if(l_count_line == 1 || l_count_square == 1 || l_count_column == 1)
+		{
+		  cout << l_transition->toString() << " weight is forced to 1 " << endl ;
+		  l_weight = 1;
+		}
+	}
+      
+      p_vector.push_back(new FSM_weighted_transition_index(l_index,l_weight));
     }
 
 }
